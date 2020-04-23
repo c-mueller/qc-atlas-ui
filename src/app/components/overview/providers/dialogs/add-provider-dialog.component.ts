a6e4172259ad2ddd05fb310b6da79f850ebae302
Tobias Mathony <mathony.tobias@gmail.com>
2020-04-23 16:46:49 +0200

    add validator for provider

--- src/app/components/overview/providers/dialogs/add-provider-dialog.component.ts
+++ src/app/components/overview/providers/dialogs/add-provider-dialog.component.ts
@@ -1,19 +1,51 @@
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-provider-dialog-component',
   templateUrl: 'add-provider-dialog.html'
 })
-export class AddProviderDialogComponent {
+export class AddProviderDialogComponent implements OnInit {
+
+  providerForm: FormGroup;
 
   constructor(
     public dialogRef: MatDialogRef<AddProviderDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData) {
   }
 
+  get name() {
+    return this.providerForm.get('name');
+  }
+
+  get accessKey() {
+    return this.providerForm.get('accessKey');
+  }
+
+  get secretKey() {
+    return this.providerForm.get('secretKey');
+  }
+
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  ngOnInit(): void {
+    this.providerForm = new FormGroup({
+      'name': new FormControl(this.data.name, [
+        Validators.required,
+        Validators.maxLength(255)
+      ]),
+      'accessKey': new FormControl(this.data.accessKey, [
+        Validators.required,
+        Validators.maxLength(255)
+      ]),
+      'secretKey': new FormControl(this.data.secretKey, [
+        Validators.required,
+        Validators.maxLength(255)
+      ])
+    });
   }
 
 }

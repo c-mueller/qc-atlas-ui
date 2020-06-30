632153dfa6b313b6f9a573ebb9e6ce90a2fe10d3
Valentin Seifermann <valesayfa@t-online.de>
2020-06-30 13:25:00 +0200

    added search bar + functions

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  HostListener,
+  ViewChild,
+} from '@angular/core';
 import { SelectionModel } from '@angular/cdk/collections';
+import { MdbTableDirective } from 'angular-bootstrap-md';
 
 @Component({
   selector: 'app-table',
@@ -17,10 +26,33 @@ export class TableComponent implements OnInit {
   @Output() selectionChange = new EventEmitter<any[]>();
   selection = new SelectionModel<any>(true, []);
   displayedColumns: string[] = [];
+  @ViewChild(MdbTableDirective, { static: true }) mdbTable: MdbTableDirective;
+  searchText = '';
+  previous: string;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.mdbTable.setDataSource(this.data);
+    this.previous = this.mdbTable.getDataSource();
+  }
+
+  @HostListener('input')
+  oninput() {
+    this.searchItems();
+  }
+
+  searchItems() {
+    const prev = this.mdbTable.getDataSource();
+    if (!this.searchText) {
+      this.mdbTable.setDataSource(this.previous);
+      this.data = this.mdbTable.getDataSource();
+    }
+    if (this.searchText) {
+      this.data = this.mdbTable.searchLocalDataBy(this.searchText);
+      this.mdbTable.setDataSource(prev);
+    }
+  }
 
   isAllSelected() {
     return this.data.length === this.selection.selected.length;

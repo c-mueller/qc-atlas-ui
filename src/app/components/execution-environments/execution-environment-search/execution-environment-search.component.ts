eb58be0acbfbd3becb9069b8e9e3ce8a13125169
Tobias Haller <tobias_haller@outlook.de>
2020-07-19 17:30:53 +0200

    Linter fix

--- src/app/components/execution-environments/execution-environment-search/execution-environment-search.component.ts
+++ src/app/components/execution-environments/execution-environment-search/execution-environment-search.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-execution-environment-search',
   templateUrl: './execution-environment-search.component.html',
-  styleUrls: ['./execution-environment-search.component.scss']
+  styleUrls: ['./execution-environment-search.component.scss'],
 })
 export class ExecutionEnvironmentSearchComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

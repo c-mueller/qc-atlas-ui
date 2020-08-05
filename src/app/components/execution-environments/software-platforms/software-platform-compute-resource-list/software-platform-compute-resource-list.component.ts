3b5db6b85b37e57fdd3c76a4904e177af9d7de1a
Tobias Haller <tobias_haller@outlook.de>
2020-08-05 13:31:20 +0200

    Added other link lists for software platform

--- src/app/components/execution-environments/software-platforms/software-platform-compute-resource-list/software-platform-compute-resource-list.component.ts
+++ src/app/components/execution-environments/software-platforms/software-platform-compute-resource-list/software-platform-compute-resource-list.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { EntityModelSoftwarePlatformDto } from 'api/models/entity-model-software-platform-dto';
+import { ExecutionEnvironmentsService } from 'api/services/execution-environments.service';
+import { Router } from '@angular/router';
+import { EntityModelComputeResourceDto } from 'api/models/entity-model-compute-resource-dto';
+import { EntityModelCloudServiceDto } from 'api/models/entity-model-cloud-service-dto';
+import { ComputeResourceDto } from 'api/models/compute-resource-dto';
+import {
+  DeleteParams,
+  LinkObject,
+} from '../../../generics/data-list/data-list.component';
 
 @Component({
   selector: 'app-software-platform-compute-resource-list',
@@ -6,7 +16,110 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./software-platform-compute-resource-list.component.scss'],
 })
 export class SoftwarePlatformComputeResourceListComponent implements OnInit {
-  constructor() {}
+  @Input() softwarePlatform: EntityModelSoftwarePlatformDto;
+  computeResources: EntityModelComputeResourceDto[];
+  linkedComputeResources: EntityModelComputeResourceDto[] = [];
 
-  ngOnInit(): void {}
+  tableColumns = ['Name', 'Vendor', 'Technology', 'Quantum Computation Model'];
+  variableNames = ['name', 'vendor', 'technology', 'quantumComputationModel'];
+  linkObject: LinkObject = {
+    title: 'Link software platform with ',
+    subtitle: 'Search compute resources by name',
+    displayVariable: 'name',
+    data: [],
+  };
+  tableAddAllowed = true;
+  isLinkingEnabled = false;
+
+  constructor(
+    private executionEnvironmentsService: ExecutionEnvironmentsService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.linkObject.title += this.softwarePlatform.name;
+    this.getComputeResources();
+    this.getLinkedComputeResources({ id: this.softwarePlatform.id });
+  }
+
+  getComputeResources(): void {
+    this.executionEnvironmentsService
+      .getComputeResources({ page: -1 })
+      .subscribe((computeResources) => {
+        if (computeResources._embedded) {
+          this.computeResources = computeResources._embedded.computeResources;
+        } else {
+          this.computeResources = [];
+        }
+      });
+  }
+
+  getLinkedComputeResources(params: any): void {
+    this.executionEnvironmentsService
+      .getComputeResourcesForSoftwarePlatform(params)
+      .subscribe((computeResource) => {
+        if (computeResource._embedded) {
+          this.linkedComputeResources =
+            computeResource._embedded.computeResources;
+        } else {
+          this.linkedComputeResources = [];
+        }
+      });
+  }
+
+  searchUnlinkedComputeResources(search: string): void {
+    if (search) {
+      search = search.toLocaleLowerCase();
+      this.linkObject.data = this.computeResources.filter(
+        (computeResource: EntityModelComputeResourceDto) =>
+          computeResource.name.toLocaleLowerCase().startsWith(search) &&
+          !this.linkedComputeResources.includes(computeResource)
+      );
+    } else {
+      this.linkObject.data = [];
+    }
+  }
+
+  linkComputeResource(computeResource: ComputeResourceDto): void {
+    this.linkObject.data = [];
+    this.executionEnvironmentsService
+      .addComputeResourceReferenceToSoftwarePlatform({
+        id: this.softwarePlatform.id,
+        crId: computeResource.id,
+      })
+      .subscribe((data) => {
+        this.getLinkedComputeResources({ id: this.softwarePlatform.id });
+      });
+  }
+
+  async unlinkComputeResources(event: DeleteParams): Promise<void> {
+    for (const computeResource of event.elements) {
+      await this.executionEnvironmentsService
+        .deleteComputeResourceReferenceFromSoftwarePlatform({
+          id: this.softwarePlatform.id,
+          crId: computeResource.id,
+        })
+        .toPromise();
+      this.getLinkedComputeResources({ id: this.softwarePlatform.id });
+    }
+  }
+
+  onAddElement(): void {}
+
+  onDatalistConfigChanged(): void {
+    this.getLinkedComputeResources({ id: this.softwarePlatform.id });
+  }
+
+  onElementClicked(computeResource: ComputeResourceDto): void {
+    this.router.navigate([
+      'execution-environments',
+      'compute-resources',
+      computeResource.id,
+    ]);
+  }
+
+  onToggleLink(): void {
+    this.isLinkingEnabled = !this.isLinkingEnabled;
+    this.tableAddAllowed = !this.tableAddAllowed;
+  }
 }
--- src/app/components/execution-environments/software-platforms/software-platform-impl-list/software-platform-impl-list.component.ts
+++ src/app/components/execution-environments/software-platforms/software-platform-impl-list/software-platform-impl-list.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { EntityModelSoftwarePlatformDto } from 'api/models/entity-model-software-platform-dto';
+import { ExecutionEnvironmentsService } from 'api/services/execution-environments.service';
+import { Router } from '@angular/router';
+import { EntityModelImplementationDto } from 'api/models/entity-model-implementation-dto';
+import { ImplementationDto } from 'api/models/implementation-dto';
+import {
+  DeleteParams,
+  LinkObject,
+} from '../../../generics/data-list/data-list.component';
 
 @Component({
   selector: 'app-software-platform-impl-list',
@@ -6,7 +15,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./software-platform-impl-list.component.scss'],
 })
 export class SoftwarePlatformImplListComponent implements OnInit {
-  constructor() {}
+  @Input() softwarePlatform: EntityModelSoftwarePlatformDto;
+  implementations: EntityModelImplementationDto[];
+  linkedImplementations: EntityModelImplementationDto[] = [];
 
-  ngOnInit(): void {}
+  variableNames: string[] = ['name', 'description', 'dependencies'];
+  tableColumns: string[] = ['Name', 'Description', 'Dependencies'];
+  linkObject: LinkObject = {
+    title: 'Link software platform with ',
+    subtitle: 'Search implementation by name',
+    displayVariable: 'name',
+    data: [],
+  };
+  tableAddAllowed = true;
+  isLinkingEnabled = false;
+
+  constructor(
+    private executionEnvironmentsService: ExecutionEnvironmentsService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.linkObject.title += this.softwarePlatform.name;
+    this.getImplementation();
+    this.getLinkedImplementations({ id: this.softwarePlatform.id });
+  }
+
+  getImplementation(): void {}
+
+  getLinkedImplementations(params: any): void {}
+
+  searchUnlinkedImplementations(search: string): void {
+    if (search) {
+      search = search.toLocaleLowerCase();
+      this.linkObject.data = this.implementations.filter(
+        (implementation: EntityModelImplementationDto) =>
+          implementation.name.toLocaleLowerCase().startsWith(search) &&
+          !this.linkedImplementations.includes(implementation)
+      );
+    } else {
+      this.linkObject.data = [];
+    }
+  }
+
+  linkImplementation(implementation: ImplementationDto): void {
+    this.linkObject.data = [];
+  }
+
+  async unlinkImplementations(event: DeleteParams): Promise<void> {
+    for (const implementation of event.elements) {
+      await this.executionEnvironmentsService
+        .deleteCloudServiceReferenceFromSoftwarePlatform({
+          id: this.softwarePlatform.id,
+          csId: implementation.id,
+        })
+        .toPromise();
+      this.getLinkedImplementations({ id: this.softwarePlatform.id });
+    }
+  }
+
+  onAddElement(): void {}
+
+  onDatalistConfigChanged(): void {
+    this.getLinkedImplementations({ id: this.softwarePlatform.id });
+  }
+
+  onElementClicked(implementation: ImplementationDto): void {
+    this.router.navigate(['algorithms', 'implementations', implementation.id]);
+  }
+
+  onToggleLink(): void {
+    this.isLinkingEnabled = !this.isLinkingEnabled;
+    this.tableAddAllowed = !this.tableAddAllowed;
+  }
 }

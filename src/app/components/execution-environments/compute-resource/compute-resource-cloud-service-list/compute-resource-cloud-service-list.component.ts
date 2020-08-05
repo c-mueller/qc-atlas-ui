95a0d6f9b30941cbd597dfdaa69ff3da6eedc3b7
Tobias Haller <tobias_haller@outlook.de>
2020-08-05 14:47:59 +0200

    Implemented reference lists for compute resources

--- src/app/components/execution-environments/compute-resource/compute-resource-cloud-service-list/compute-resource-cloud-service-list.component.ts
+++ src/app/components/execution-environments/compute-resource/compute-resource-cloud-service-list/compute-resource-cloud-service-list.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { EntityModelComputeResourceDto } from 'api/models/entity-model-compute-resource-dto';
+import { EntityModelCloudServiceDto } from 'api/models/entity-model-cloud-service-dto';
+import { ExecutionEnvironmentsService } from 'api/services/execution-environments.service';
+import { Router } from '@angular/router';
+import { CloudServiceDto } from 'api/models/cloud-service-dto';
+import {
+  DeleteParams,
+  LinkObject,
+} from '../../../generics/data-list/data-list.component';
 
 @Component({
   selector: 'app-compute-resource-cloud-service-list',
@@ -6,7 +15,109 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compute-resource-cloud-service-list.component.scss'],
 })
 export class ComputeResourceCloudServiceListComponent implements OnInit {
-  constructor() {}
+  @Input() computeResource: EntityModelComputeResourceDto;
+  cloudServices: EntityModelCloudServiceDto[];
+  linkedCloudServices: EntityModelCloudServiceDto[] = [];
 
-  ngOnInit(): void {}
+  tableColumns = ['Name', 'Provider', 'Description', 'CostModel', 'URL'];
+  variableNames = ['name', 'provider', 'description', 'costModel', 'URL'];
+  linkObject: LinkObject = {
+    title: 'Link software platform with ',
+    subtitle: 'Search cloud services by name',
+    displayVariable: 'name',
+    data: [],
+  };
+  tableAddAllowed = true;
+  isLinkingEnabled = false;
+
+  constructor(
+    private executionEnvironmentsService: ExecutionEnvironmentsService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.linkObject.title += this.computeResource.name;
+    this.getCloudServices();
+    this.getLinkedCloudServices({ id: this.computeResource.id });
+  }
+
+  getCloudServices(): void {
+    this.executionEnvironmentsService
+      .getCloudServices({ page: -1 })
+      .subscribe((cloudServices) => {
+        if (cloudServices._embedded) {
+          this.cloudServices = cloudServices._embedded.cloudServices;
+        } else {
+          this.cloudServices = [];
+        }
+      });
+  }
+
+  getLinkedCloudServices(params: any): void {
+    this.executionEnvironmentsService
+      .getCloudServicesForComputeResource(params)
+      .subscribe((cloudServices) => {
+        if (cloudServices._embedded) {
+          this.linkedCloudServices = cloudServices._embedded.cloudServices;
+        } else {
+          this.linkedCloudServices = [];
+        }
+      });
+  }
+
+  searchUnlinkedCloudServices(search: string): void {
+    if (search) {
+      search = search.toLocaleLowerCase();
+      this.linkObject.data = this.cloudServices.filter(
+        (cloudService: EntityModelCloudServiceDto) =>
+          cloudService.name.toLocaleLowerCase().startsWith(search) &&
+          !this.linkedCloudServices.includes(cloudService)
+      );
+    } else {
+      this.linkObject.data = [];
+    }
+  }
+
+  linkCloudService(cloudService: CloudServiceDto): void {
+    this.linkObject.data = [];
+    this.executionEnvironmentsService
+      .addComputeResourceReferenceToCloudService({
+        id: cloudService.id,
+        crId: this.computeResource.id,
+      })
+      .subscribe((data) => {
+        this.getLinkedCloudServices({ id: this.computeResource.id });
+      });
+  }
+
+  async unlinkCloudServices(event: DeleteParams): Promise<void> {
+    for (const cloudService of event.elements) {
+      await this.executionEnvironmentsService
+        .deleteComputeResourceReferenceFromCloudService({
+          id: cloudService.id,
+          crId: this.computeResource.id,
+        })
+        .toPromise();
+      this.getLinkedCloudServices({ id: this.computeResource.id });
+    }
+  }
+
+  onAddElement(): void {}
+
+  onDatalistConfigChanged(): void {
+    this.getLinkedCloudServices({ id: this.computeResource.id });
+  }
+
+  onElementClicked(cloudService: CloudServiceDto): void {
+    this.router.navigate([
+      'execution-environments',
+      'cloud-services',
+      cloudService.id,
+    ]);
+  }
+
+  onToggleLink(): void {
+    this.isLinkingEnabled = !this.isLinkingEnabled;
+    this.tableAddAllowed = !this.tableAddAllowed;
+  }
 }
--- src/app/components/execution-environments/compute-resource/compute-resource-software-platform-list/compute-resource-software-platform-list.component.ts
+++ src/app/components/execution-environments/compute-resource/compute-resource-software-platform-list/compute-resource-software-platform-list.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { EntityModelSoftwarePlatformDto } from 'api/models/entity-model-software-platform-dto';
+import { ExecutionEnvironmentsService } from 'api/services/execution-environments.service';
+import { Router } from '@angular/router';
+import { SoftwarePlatformDto } from 'api/models/software-platform-dto';
+import { EntityModelComputeResourceDto } from 'api/models/entity-model-compute-resource-dto';
+import {
+  DeleteParams,
+  LinkObject,
+} from '../../../generics/data-list/data-list.component';
 
 @Component({
   selector: 'app-compute-resource-software-platform-list',
@@ -6,7 +15,111 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compute-resource-software-platform-list.component.scss'],
 })
 export class ComputeResourceSoftwarePlatformListComponent implements OnInit {
-  constructor() {}
+  @Input() computeResource: EntityModelComputeResourceDto;
+  softwarePlatforms: EntityModelSoftwarePlatformDto[];
+  linkedSoftwarePlatforms: EntityModelSoftwarePlatformDto[] = [];
 
-  ngOnInit(): void {}
+  tableColumns = ['Name', 'Version', 'Licence', 'Link'];
+  variableNames = ['name', 'version', 'licence', 'link'];
+  linkObject: LinkObject = {
+    title: 'Link compute resource with ',
+    subtitle: 'Search software platform by name',
+    displayVariable: 'name',
+    data: [],
+  };
+  tableAddAllowed = true;
+  isLinkingEnabled = false;
+
+  constructor(
+    private executionEnvironmentsService: ExecutionEnvironmentsService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.linkObject.title += this.computeResource.name;
+    this.getSoftwarePlatforms();
+    this.getLinkedSoftwarePlatforms({ id: this.computeResource.id });
+  }
+
+  getSoftwarePlatforms(): void {
+    this.executionEnvironmentsService
+      .getSoftwarePlatforms({ page: -1 })
+      .subscribe((softwarePlatforms) => {
+        if (softwarePlatforms._embedded) {
+          this.softwarePlatforms =
+            softwarePlatforms._embedded.softwarePlatforms;
+        } else {
+          this.softwarePlatforms = [];
+        }
+      });
+  }
+
+  getLinkedSoftwarePlatforms(params: any): void {
+    this.executionEnvironmentsService
+      .getSoftwarePlatformsForComputeResource(params)
+      .subscribe((softwarePlatforms) => {
+        if (softwarePlatforms._embedded) {
+          this.linkedSoftwarePlatforms =
+            softwarePlatforms._embedded.softwarePlatforms;
+        } else {
+          this.linkedSoftwarePlatforms = [];
+        }
+      });
+  }
+
+  searchUnlinkedSoftwarePlatforms(search: string): void {
+    if (search) {
+      search = search.toLocaleLowerCase();
+      this.linkObject.data = this.softwarePlatforms.filter(
+        (softwarePlatform: EntityModelSoftwarePlatformDto) =>
+          softwarePlatform.name.toLocaleLowerCase().startsWith(search) &&
+          !this.linkedSoftwarePlatforms.includes(softwarePlatform)
+      );
+    } else {
+      this.linkObject.data = [];
+    }
+  }
+
+  linkSoftwarePlatform(softwarePlatform: SoftwarePlatformDto): void {
+    this.linkObject.data = [];
+    this.executionEnvironmentsService
+      .addComputeResourceReferenceToSoftwarePlatform({
+        id: softwarePlatform.id,
+        crId: this.computeResource.id,
+      })
+      .subscribe((data) => {
+        this.getLinkedSoftwarePlatforms({ id: this.computeResource.id });
+      });
+  }
+
+  async unlinkSoftwarePlatforms(event: DeleteParams): Promise<void> {
+    for (const softwarePlatform of event.elements) {
+      await this.executionEnvironmentsService
+        .deleteComputeResourceReferenceFromSoftwarePlatform({
+          id: softwarePlatform.id,
+          crId: this.computeResource.id,
+        })
+        .toPromise();
+      this.getLinkedSoftwarePlatforms({ id: this.computeResource.id });
+    }
+  }
+
+  onAddElement(): void {}
+
+  onDatalistConfigChanged(): void {
+    this.getLinkedSoftwarePlatforms({ id: this.computeResource.id });
+  }
+
+  onElementClicked(softwarePlatform: SoftwarePlatformDto): void {
+    this.router.navigate([
+      'execution-environments',
+      'software-platforms',
+      softwarePlatform.id,
+    ]);
+  }
+
+  onToggleLink(): void {
+    this.isLinkingEnabled = !this.isLinkingEnabled;
+    this.tableAddAllowed = !this.tableAddAllowed;
+  }
 }
